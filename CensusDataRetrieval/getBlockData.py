aaf35d20755ac57a073a87dcf5209107da4fe2cb
Chris Larson <larson.chris@gmail.com>
2018-08-18 21:46:14 -0400

    Get Tracts as well and Blocks
    Block numbers aren't unique

--- CensusDataRetrieval/getBlockData.py
+++ CensusDataRetrieval/getBlockData.py
@@ -6,6 +6,6 @@ censusRequest = Census(apiKeys.censusAPIKey, year=2010)
 
 # P0010001 is the total population as defined by: https://api.census.gov/data/2010/sf1/variables.html
 # 093 is Livingston County in Michigan's FIPS code for the 2010 US Census. This is just an example to get running
-livingstonCountyBlocks = censusRequest.sf1.get(fields=('NAME', 'P0010001'), geo={'for': 'block:*', 'in':'state:{0} county:93'.format(states.MI.fips, states)})
+livingstonCountyBlocks = censusRequest.sf1.get(fields=('NAME', 'TRACT', 'P0010001'), geo={'for': 'block:*', 'in':'state:{0} county:93'.format(states.MI.fips, states)})
 
 print(livingstonCountyBlocks)
