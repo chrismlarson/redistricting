e95a1019b2dceed4be798df726403e5cc23abd8d
Chris Larson <larson.chris@gmail.com>
2018-08-21 12:29:14 -0400

    Getting all block data for the specified state by county
    Also cleaned up some of the county code

--- CensusDataRetrieval/getBlockData.py
+++ CensusDataRetrieval/getBlockData.py
@@ -2,24 +2,31 @@ from census import Census
 from us import states
 import apiKeys
 
+
 def getCountiesInState(stateFIPSCode):
-    allCountiesInState = censusRequest.sf1.get(fields=('COUNTY', 'NAME'), geo={'for': 'county:*',
-                                                                                  'in': 'state:{0}'.format(
-                                                                                      stateFIPSCode, states)})
+    allCountiesInState = censusRequest.sf1.get(fields=('NAME'), geo={'for': 'county:*',
+                                                                     'in': 'state:{0}'.format(
+                                                                         stateFIPSCode)})
     return allCountiesInState
 
+
+def getBlocksInCounty(stateFIPSCode, countyFIPSCode):
+    # P0010001 is the total population as defined by: https://api.census.gov/data/2010/sf1/variables.html
+    countyBlocks = censusRequest.sf1.get(fields=('NAME', 'P0010001'), geo={'for': 'block:*',
+                                                                           'in': 'state:{0} county:{1}'.format(
+                                                                               stateFIPSCode, countyFIPSCode)})
+    return countyBlocks
 
 
 censusRequest = Census(apiKeys.censusAPIKey, year=2010)
 
+stateFIPS = states.MI.fips
 
-print(getCountiesInState(states.MI.fips))
+countyInfoList = getCountiesInState(stateFIPSCode=stateFIPS)
+fullBlockList = []
 
-
-# P0010001 is the total population as defined by: https://api.census.gov/data/2010/sf1/variables.html
-# 093 is Livingston County in Michigan's FIPS code for the 2010 US Census. This is just an example to get running
-livingstonCountyBlocks = censusRequest.sf1.get(fields=('NAME', 'P0010001'), geo={'for': 'block:*', 'in':'state:{0} county:93'.format(states.MI.fips, states)})
-
-print(livingstonCountyBlocks)
-
-
+for county in countyInfoList:
+    countyFIPS = county['county']
+    blocksInCounty = getBlocksInCounty(stateFIPSCode=stateFIPS, countyFIPSCode=countyFIPS)
+    fullBlockList += blocksInCounty
+    print('Got info on {0}'.format(county['NAME']))
