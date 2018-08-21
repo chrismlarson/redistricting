a6a636daecbf8b40eda7219dcdfab09d45e733cb
Chris Larson <larson.chris@gmail.com>
2018-08-21 12:46:29 -0400

    Breaking out block gathering to method

--- CensusDataRetrieval/getBlockData.py
+++ CensusDataRetrieval/getBlockData.py
@@ -1,6 +1,7 @@
 from census import Census
 from us import states
 import apiKeys
+import math
 
 
 def getCountiesInState(stateFIPSCode):
@@ -9,7 +10,6 @@ def getCountiesInState(stateFIPSCode):
                                                                          stateFIPSCode)})
     return allCountiesInState
 
-
 def getBlocksInCounty(stateFIPSCode, countyFIPSCode):
     # P0010001 is the total population as defined by: https://api.census.gov/data/2010/sf1/variables.html
     countyBlocks = censusRequest.sf1.get(fields=('NAME', 'P0010001'), geo={'for': 'block:*',
@@ -17,16 +17,28 @@ def getBlocksInCounty(stateFIPSCode, countyFIPSCode):
                                                                                stateFIPSCode, countyFIPSCode)})
     return countyBlocks
 
+def getAllBlocksInState(countyList, maxNumberOfCounties=math.inf):
+    fullBlockList = []
+
+    # getting population counts and Block names for each county
+    count = 0
+    for county in countyList:
+        if count >= maxNumberOfCounties:
+            break
+        countyFIPS = county['county']
+        blocksInCounty = getBlocksInCounty(stateFIPSCode=county['state'], countyFIPSCode=countyFIPS)
+        fullBlockList += blocksInCounty
+        print('Got info on {0}'.format(county['NAME']))
+        count += 1
+
+    return fullBlockList
+
 
 censusRequest = Census(apiKeys.censusAPIKey, year=2010)
 
 stateFIPS = states.MI.fips
 
 countyInfoList = getCountiesInState(stateFIPSCode=stateFIPS)
-fullBlockList = []
+allBlocksInState = getAllBlocksInState(countyList=countyInfoList)
 
-for county in countyInfoList:
-    countyFIPS = county['county']
-    blocksInCounty = getBlocksInCounty(stateFIPSCode=stateFIPS, countyFIPSCode=countyFIPS)
-    fullBlockList += blocksInCounty
-    print('Got info on {0}'.format(county['NAME']))
+#todo: get shapefiles or neighboring blocks for each block
