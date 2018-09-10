caae86b44bc46173a79cc0157c614bcc1d85de68
Chris Larson <larson.chris@gmail.com>
2018-09-09 21:28:22 -0400

    Stubbing out shapely conversion
    still haven't installed shapely on windows

--- counties.py
+++ counties.py
@@ -1,9 +1,20 @@
+import shapely
+# On Windows, I needed to install Shapely manually
+# Found whl file here: https://www.lfd.uci.edu/~gohlke/pythonlibs/#shapely
+# And then ran:
+# from pip._internal import main
+# def install_whl(path):
+#     main(['install', path])
+# install_whl("path_to_file\\Shapely-1.6.4.post1-cp37-cp37m-win32.whl")
+# but not sure if this worked...
+
+
 class County:
     def __init__(self, countyName, countyFIPS, countyGeoJSONGeometry):
         self.name = countyName
         self.FIPS = countyFIPS
-        #todo: convert to shapely geometry for comparisons
-        self.geometry = countyGeoJSONGeometry
+        self.geometry = convertGeoJSONToShapely(countyGeoJSONGeometry)
+
 
 def createCountiesFromRawData(rawCountyData):
     counties = []
@@ -14,3 +25,7 @@ def createCountiesFromRawData(rawCountyData):
     return counties
 
 
+def convertGeoJSONToShapely(geoJSON):
+    # todo: implement
+    # try http://www.mhermans.net/geojson-shapely-geocoding.html : polygon = shape(feature['geometry'])
+    return geoJSON
