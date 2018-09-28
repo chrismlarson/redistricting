fada4b73d9b700fa0abbea2e378f6e7e597ee23f
Chris Larson <larson.chris@gmail.com>
2018-09-28 12:34:39 -0400

    Breaking out CSV methods

--- formatData/formatBlockData.py
+++ formatData/formatBlockData.py
@@ -1,49 +1,16 @@
 import csv
 from os import path
 import ast
-import sys
 from tqdm import tqdm
 from censusData import censusCounty, censusBlock
+import csvHelper
 from formatData import redistrictingGroup
 from exportData import exportData
 
 
-def getNumOfCSVRows(csvPath):
-    setCSVLimitToMaxAcceptable()
-
-    tqdm.write('*** Getting total number of CSV rows for progress ***')
-    totalNumberOfRows = 0
-    with tqdm() as pbar:
-        with open(csvPath, newline='\n') as csvFile:
-            dictReader = csv.DictReader(csvFile)
-            for row in dictReader:
-                totalNumberOfRows += 1
-                pbar.update(1)
-
-    totalNumberOfRows -= 1 #to account for the header
-    tqdm.write('Total number of CSV rows: {0}'.format(totalNumberOfRows))
-    return totalNumberOfRows
-
-
-def setCSVLimitToMaxAcceptable():
-    decrement = True
-    maxInt = sys.maxsize
-
-    while decrement:
-        # decrease the maxInt value by factor 10
-        # as long as the OverflowError occurs.
-
-        decrement = False
-        try:
-            csv.field_size_limit(maxInt)
-        except OverflowError:
-            maxInt = int(maxInt / 10)
-            decrement = True
-
-
 def getRawDictData(csvPath):
-    setCSVLimitToMaxAcceptable()
-    numOfCSVRows = getNumOfCSVRows(csvPath=csvPath)
+    csvHelper.setCSVLimitToMaxAcceptable()
+    numOfCSVRows = csvHelper.getNumOfCSVRows(csvPath=csvPath)
 
     tqdm.write('*** Loading CSV data ***')
     rawDictData = []
@@ -60,6 +27,7 @@ def getRawDictData(csvPath):
                 pbar.update(1)
 
     return rawDictData
+
 
 countyCSVPath = path.expanduser('~/Documents/2010-Michigan-ThumbPlusInghamCountyInfo.csv')
 rawCountyData = getRawDictData(csvPath=countyCSVPath)
