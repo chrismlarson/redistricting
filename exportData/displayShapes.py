4f6610255fb865796b7a2caacc70871e97479c4e
Chris Larson <larson.chris@gmail.com>
2018-10-26 15:49:21 -0400

    Drawing bordering blocks as purple

--- exportData/displayShapes.py
+++ exportData/displayShapes.py
@@ -2,9 +2,14 @@ from matplotlib import pyplot
 from descartes import PolygonPatch
 
 
-def plotBlocksFromRedistrictingGroup(redistrictingGroup):
+def plotBlocksForRedistrictingGroup(redistrictingGroup, shouldShowText=False):
+    plotBlocksForRedistrictingGroups([redistrictingGroup], shouldShowText)
+
+
+def plotBlocksForRedistrictingGroups(redistrictingGroups, shouldShowText=False):
     blueColor = '#6699cc'
     greenColor = '#66cc78'
+    purpleColor = '#b266cc'
     fig = pyplot.figure()
     ax = fig.gca()
 
@@ -13,16 +18,19 @@ def plotBlocksFromRedistrictingGroup(redistrictingGroup):
             'weight': 'normal',
             'size': 9,
             }
-    for block in redistrictingGroup.blocks:
-        if block.isWater:
-            ax.add_patch(PolygonPatch(block.geometry, fc=blueColor, ec=blueColor, alpha=0.5, zorder=2 ))
-        else:
-            ax.add_patch(PolygonPatch(block.geometry, fc=greenColor, ec=greenColor, alpha=0.5, zorder=2))
-        #todo: add text for poplation size
-        centerOfBlock = block.geometry.centroid
-        ax.text(x=centerOfBlock.x, y=centerOfBlock.y, s=block.population, fontdict=font)
-        # ax.text()
-        # https://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.text
+    for redistrictingGroup in redistrictingGroups:
+        for block in redistrictingGroup.blocks:
+            if block.isWater:
+                ax.add_patch(PolygonPatch(block.geometry, fc=blueColor, ec=blueColor, alpha=0.5, zorder=2 ))
+            else:
+                if redistrictingGroup.isBorderBlock(block):
+                    ax.add_patch(PolygonPatch(block.geometry, fc=purpleColor, ec=purpleColor, alpha=0.5, zorder=2))
+                else:
+                    ax.add_patch(PolygonPatch(block.geometry, fc=greenColor, ec=greenColor, alpha=0.5, zorder=2))
+            centerOfBlock = block.geometry.centroid
+
+            if shouldShowText:
+                ax.text(x=centerOfBlock.x, y=centerOfBlock.y, s=block.population, fontdict=font)
 
     ax.axis('scaled')
     pyplot.show()
